0aeac3641540198dbaf2844146543ebb99e419a9
Victor Holanda <jvictorweb21@gmail.com>
2015-06-10 22:00:12 -0300

    exer02quest09

--- exercicios/exercicio02/quest09.rb
+++ exercicios/exercicio02/quest09.rb
@@ -1,12 +1,7 @@
 num = []
-print "Digite o primeiro numero: "
-num[0] = gets.chomp.to_f
-print "Digite o segundo numero: "
-num[1] = gets.chomp.to_f
-print "Digite o terceiro numero: "
-num[2] = gets.chomp.to_f
-
-num.sort!.reverse!
 for i in (0..2)
-	puts num[i]
+	print "Digite o #{i+1}º número: "
+	num[i] = gets.chomp.to_f
 end
+num.sort!.reverse!
+puts num

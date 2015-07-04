725f3578c79efdad3554e852bcb7d9738167e7c4
Victor Holanda <victorweb21@hotmail.com>
2015-07-04 17:52:21 -0300

    aula04deJulho

--- Apps/blog/db/schema.rb
+++ Apps/blog/db/schema.rb
@@ -11,21 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627170233) do
+ActiveRecord::Schema.define(version: 20150704193822) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text   "body"
+    t.string "name"
+    t.string "email"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "site"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "author_id"
   end
 
 end
